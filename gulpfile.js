04607a6d91b4b3c1a44eb84bea27e5c2dc51e8c9
Pramod Dhokane <Pramod Dhokane>
2016-02-14 16:07:50 +0530

    Added Jshint and JSCS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
 
+var jshint=require('gulp-jshint');
+var jscs=require('gulp-jscs');
+
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('vet',function(){
+    return gulp
+    .src([
+        './src/**/*.js',
+        './*.js'
+    ])
+    .pipe(jscs())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish',{verbose:true}));
+    
 });
