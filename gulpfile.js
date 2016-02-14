aca86782e7bb3ce63f28b5a8f27f2c7cacdcd051
Pramod Dhokane <Pramod Dhokane>
2016-02-14 18:53:15 +0530

    Added new gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-var gulp = require('gulp');
-
-var jshint=require('gulp-jshint');
-var jscs=require('gulp-jscs');
-
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    gulpprint = require('gulp-print'),
+    gulpif = require('gulp-if'),
+    args = require('yargs').argv;
 gulp.task('default', function() {
-  // place code for your default task here
+    //place code for your default task here
 });
 
-gulp.task('vet',function(){
+gulp.task('vet',function() {
     return gulp
     .src([
         './src/**/*.js',
         './*.js'
     ])
+    .pipe(gulpif(args.verbose,gulpprint()))
     .pipe(jscs())
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish',{verbose:true}));
-   
 });
